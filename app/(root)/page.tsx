8311e60ff17ba7c2beb792f14b5d0b11f11e9b28
cary <cary@zhuoruint.com>
2025-06-24 11:44:08 +0800

    feat: 数据实时更新

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,11 @@
 import SearchForm from "../../components/SearchForm";
 import StartupCard from "../../components/StartupCard";
-import { client } from "@/sanity/lib/client";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({searchParams}: {searchParams: Promise<{query: string}>}) {
   const query = (await searchParams).query;
-  const posts = await client.fetch(STARTUPS_QUERY);
+  const {data: posts} = await sanityFetch({query: STARTUPS_QUERY});
 
   return (
     <>
@@ -35,6 +35,7 @@ export default async function Home({searchParams}: {searchParams: Promise<{query
         }
         </ul>
     </section>
+    <SanityLive />
     </>
   );
 }
